b606a5c04bffc5db40da16ed1c8cce922faeb35c
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2023-08-25 04:48:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const chalk = require('chalk');
 const { prompt } = require('./src/prompt.js');
-const { igCreate } = require('./src/instagramCreate.js');
-const { igEditBio, igFollowFollowers } = require('./src/instagramTools.js');
+const { createAccount, uploadProfile, editBio, checktarget, follow } = require('./src/instagram.js');
 
 if (!fs.existsSync('../storage/downloads/hasil_akun_ig.txt')) {fs.appendFileSync('../storage/downloads/hasil_akun_ig.txt', '')};
 if (!fs.existsSync('../storage/downloads/bio_text.txt')) {fs.appendFileSync('../storage/downloads/bio_text.txt', '')};
@@ -11,36 +10,74 @@ if (!fs.existsSync('../storage/downloads/akun_target.txt')) {fs.appendFileSync('
 
 process.stdout.write('\033c');
 
-const create = async (modeCreate, modeAgent) => {
-    var toolsCreate = await igCreate(modeCreate, modeAgent);
-
-    if (toolsCreate !== false) {
-        if (modeCreate === 3) {
-            const bio = getBio();
-            const link = getLink();
-            const target = getTarget();
-            const setBio = await igEditBio(bio, link, toolsCreate);
-            if (setBio && setBio.user && setBio.user.biography) {
-                console.log(chalk`{bold.white Update Bio {bold.green Success}}`);
+const main = async (a) => {
+    const create = await createAccount(a);
+    if (create !== false) {
+        if (a === 1) {
+            fs.appendFileSync('../storage/downloads/hasil_akun_ig.txt', create.username + '|' + create.password + '|' + create.email + '|' + create.cookies + '\n');
+            console.log(chalk`{bold.white ========================================}`);
+            main(a);
+        } else if (a === 2) {
+            const upload = await uploadProfile(create.cookies);
+            if (upload === true) {
+                console.log(chalk`{bold.white ✔ Profile: {bold.green Success}}`);
+                console.log(chalk`{bold.white ========================================}`);
+                main(a);
             } else {
-                console.log(chalk`{bold.white Update Bio {bold.red Failed}}`);
+                console.log(chalk`{bold.white ✘ Profile: {bold.red Failed}}`);
+                console.log(chalk`{bold.white ========================================}`);
+                main(a);
             }
-            await igFollowFollowers(target, toolsCreate);
-            create(modeCreate, modeAgent);
-        } else {
-            create(modeCreate, modeAgent);
+        } else if (a === 3) {
+            const bio = await getBio();
+            const link = await getLink();
+            const target = await getTarget();
+
+            const uploadProfile = await uploadProfile(create.cookies);
+            const updateBio = await editBio(create.cookies, {'bio': bio, 'link': link});
+
+            uploadProfile = uploadProfile ? `✔ Profile: ${chalk.bold.green('Success')}` : `✘ Profile: ${chalk.bold.red('Failed')}`;
+            updateBio = updateBio ? `✔ Bio: ${chalk.bold.green('Success')}` : `✘ Bio: ${chalk.bold.red('Failed')}`;
+
+            console.log(chalk`{bold.white ${uploadProfile}}`);
+            console.log(chalk`{bold.white ${updateBio}}`);
+            console.log(chalk`{bold.white ========================================}`);
+
+            const checkTarget = await checktarget(create.cookies, {'target': target});
+            if (checkTarget !== false && checkTarget.is_private === false) {
+                const grab = await follow(create.cookies, {'mode': 'followers', 'target': target});
+                if (grab === true) {
+                    console.log(chalk`{bold.green Follow Complete}`);
+                    console.log(chalk`{bold.white ========================================}`);
+                    main(a);
+                } else {
+                    console.log(chalk`{bold.yellow Account Limit}`);
+                    console.log(chalk`{bold.white ========================================}`);
+                    main(a);
+                }
+            } else if (checkTarget !== false && checkTarget.is_private === true) {
+                console.log(chalk`{bold.yellow Target ${checkTarget} is Private}`);
+                console.log(chalk`{bold.white ========================================}`);
+                main(a);
+            } else {
+                console.log(chalk`{bold.red Auto Follow Failed}`);
+                console.log(chalk`{bold.white ========================================}`);
+                main(a);
+            }
         }
     } else {
-        create(modeCreate, modeAgent);
+        console.log(chalk`{bold.white ========================================}`);
+        main(a);
     }
 }
 
-function getBio() {
+const getBio = async () => {
     var data = fs.readFileSync('../storage/downloads/bio_text.txt', 'utf8');
-    return data.trim();
+    var lines = data.split('\r\n');
+    return lines.join('%5Cn');
 }
 
-function getLink() {
+const getLink = async () => {
     var data = fs.readFileSync('../storage/downloads/bio_link.txt', 'utf8');
     var lines = data.split('\n');
     if (lines.length > 0 && lines[0] !== '') {
@@ -53,7 +90,7 @@ function getLink() {
     }
 }
 
-function getTarget() {
+const getTarget = async () => {
     var data = fs.readFileSync('../storage/downloads/akun_target.txt', 'utf8');
     var lines = data.split('\n');
     if (lines.length > 0 && lines[0] !== '') {
@@ -68,5 +105,5 @@ function getTarget() {
 
 (async () => {
     const mode = await prompt();
-    create(mode.modeCreate, mode.modeAgent);
+    main(mode.modeCreate);
 })();
