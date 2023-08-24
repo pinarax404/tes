96852779abf0150238ea879fa046013c5d3a0f57
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2023-08-24 18:12:26 +0700

    Update instagramTools.js

--- src/instagramTools.js
+++ src/instagramTools.js
@@ -9,7 +9,7 @@ var header = {
     'x-ig-www-claim': '0',
     'x-instagram-ajax': '1008220018',
     'x-csrftoken': '',
-    'user-agent': 'Mozilla/5.0 (Linux; U; Android 2.3.6; fa-fa; GT-S5570I Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 7.1.2; SM-G935FD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
     'cookie': ''
 }
 
