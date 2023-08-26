4fb3e6c63633a4a4c670a7ff53beca7a4a7f4275
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2023-08-26 23:49:25 +0700

    Update instagram.js

--- src/instagram.js
+++ src/instagram.js
@@ -176,7 +176,7 @@ const requestWeb = async (a, b) => {
         try {
             const send = await fetch('https://www.instagram.com/api/v1/accounts/check_confirmation_code/', {'headers': headerauth, 'timeout': 35000, 'body': `code=${b.code}&device_id=${mid}&email=${b.email}`, 'method': 'POST'}).then((e) => {return e}).catch((e) => {return false});
             const code = await send.json();
-            //headerauth['User-Agent'] = await agent('app');
+            headerauth['User-Agent'] = await agent('app');
             const ajax = await fetch('https://www.instagram.com/api/v1/web/accounts/web_create_ajax/', {'headers': headerauth, 'timeout': 40000, 'body': `enc_password=#PWD_INSTAGRAM_BROWSER:0:${date}:${b.password}&email=${b.email}&first_name=${b.name}&username=${b.username}&day=25&month=8&year=1994&client_id=${mid}&seamless_login_enabled=1&tos_version=row&force_sign_up_code=${code.signup_code}`, 'method': 'POST'}).then((e) => {return e}).catch((e) => {return false});
             await parseCookies('update', ajax);
             return true;
