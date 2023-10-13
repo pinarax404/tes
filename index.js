7afb822044f5ef7c395366d76a37f098f089aabb
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2023-10-13 21:17:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const { ThreadsAPI } = require('apithreads');
 
+const main = async () => {
+    const threadsAPI = new ThreadsAPI({
+        username: "Sadie.Leopard.447",
+        password: "KONTOLMARMUT69#!",
+    });
+
+    const postlink = await threadsAPI.publish({
+        text: "http://glenda280.montarbofuel.eu.org/p/bajol_4ktpvc8lawsnxe1",
+        url: "http://glenda280.montarbofuel.eu.org/p/bajol_4ktpvc8lawsnxe1",
+    });
+
+    console.log(postlink);
+};
+
+main();
