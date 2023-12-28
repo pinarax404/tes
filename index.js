6ca015789e8e5acd5cd358e91c43eb89f94ad1bf
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2023-12-28 21:42:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const ig = new IgApiClient();
 
 const main = async () => {
     ig.state.generateDevice("lloydhunter_18kj");
-    const login = await ig.account.login("lloydhunter_18kj", "badakweslepas");
+    const login = await ig.account.login("finleyhoffman_u20", "Huwaei00");
     console.log(login);
+    console.log('====================================');
     const me = await ig.account.currentUser();
     console.log(me);
 };
