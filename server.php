fec9c1d2eb0d324fc39164e7f1993fb4ca46bfb2
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2024-11-04 04:29:15 +0700

    Update server.php

--- server.php
+++ server.php
@@ -3,7 +3,7 @@
 shell_exec("su -c 'settings put global airplane_mode_on 1'");
 shell_exec("su -c 'am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true'");
 
-sleep(5);
+sleep(1);
 
 shell_exec("su -c 'settings put global airplane_mode_on 0'");
 shell_exec("su -c 'am broadcast -a android.intent.action.AIRPLANE_MODE --ez state false'");
