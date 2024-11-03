e17e76b89b904eaab06091c0ecf0dcb60b52b110
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2024-11-04 05:45:39 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const serverOn = async () => {
+    server.listen(3000, () => {
+        console.log('Server Running...');
+    });
+
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.get('/', function(req, res) {
+        console.log('Connected');
+    });
+}
