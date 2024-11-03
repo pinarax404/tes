28bf6493e43cfc1ceb01e6851136609c81a709ea
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2024-11-04 05:51:17 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = express();
 const server = require('http').Server(app);
 const bodyParser = require('body-parser');
 const path = require('path');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
 
 const serverOn = async () => {
     server.listen(3000, () => {
@@ -12,7 +14,10 @@ const serverOn = async () => {
     app.use(bodyParser.urlencoded({extended: true}));
 
     app.get('/', function(req, res) {
+        await exec("su -c 'settings put global airplane_mode_on 1'");
+        await exec("su -c 'am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true'");
         console.log('Connected');
+        res.send('Done');
     });
 }
 
