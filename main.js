ade40f8307b5facf1c3d27d4177a860e47704e14
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2024-11-04 22:27:32 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,23 +7,8 @@ const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 
 const serverOn = async () => {
-    server.listen(3000, () => {
-        console.log('Server Running...');
-    });
-
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(express.static(path.join(__dirname, './www')));
-
-    app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, './www', 'index.html'));
-    });
-
-    app.get('/phone_config', async function(req, res) {
-
-        const b = await exec("shell dumpsys wifi | grep -i ssid");
-        console.log(b);
-        res.send(b);
-    });
+    const b = await exec("su -c 'settings list system'");
+    console.log(b);
 }
 
 serverOn();
