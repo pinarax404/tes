a11093be93129854bad8e2a534d0f69f5e17bc05
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2024-11-04 22:22:48 +0700

    Update main.js

--- main.js
+++ main.js
@@ -19,30 +19,10 @@ const serverOn = async () => {
     });
 
     app.get('/phone_config', async function(req, res) {
-        let dump = {mobile_data: '', wifi: '', airplane_mode: '', battery: ''};
 
-        const tes = await exec("getprop gsm.sim.operator.alpha");
-        console.log(tes);
-
-        
-        const a = await exec("su -c 'settings get global mobile_data'");
-        const mobile_data = a.stdout.split('\n');
-        dump['mobile_data'] = mobile_data[0];
-
-        const b = await exec("su -c 'settings get global wifi_on'");
-        const wifi_on = b.stdout.split('\n');
-        dump['wifi'] = wifi_on[0];
-
-        const c = await exec("su -c 'settings get global airplane_mode_on'");
-        const airplane_mode_on = c.stdout.split('\n');
-        dump['airplane_mode'] = airplane_mode_on[0];
-
-        const d = await exec("cat /sys/class/power_supply/battery/capacity");
-        const battery = d.stdout.split('\n');
-        dump['battery'] = battery[0];
-
-        console.log(dump);
-        res.send(dump);
+        const b = await exec("am start -n com.android.phone/com.android.phone.Settings");
+        console.log(b);
+        res.send(b);
     });
 }
 
