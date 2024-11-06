07b0647b4bad654f4cd33f588729b7b86bf01235
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2024-11-07 04:50:42 +0700

    Update main.js

--- www/js/main.js
+++ www/js/main.js
@@ -79,21 +79,22 @@ function updateWifiList() {
     let count = 0;
     load();
     function load() {
-        $.getJSON('/scanWifi', function(res) {
-            if (res.status === 'ok') {
-                if (count != res.wifi.length) {
-                    count = res.wifi.length;
-                    document.getElementById('modalResult').innerHTML = '';
-                    for (let i = 0; i < res.wifi.length; i++) {
-                        document.getElementById('modalResult').innerHTML += `<span type="button" class="btn btn-white w-100 align-items-stretch d-flex" style="width: 358px; background-color: #EBEBEB;"><b style="float: left;"><i class="fa fa-wifi" id="wifi_icon" style="color: dodgerblue;"></i> ${res.wifi[i].ssid}</b><b style="float: right;">Connect</b></span>`;
+        const cls = document.getElementById('mainModal').getAttribute('class');
+        if (cls === 'modal fade in') {
+            $.getJSON('/scanWifi', function(res) {
+                if (res.status === 'ok') {
+                    if (count != res.wifi.length) {
+                        count = res.wifi.length;
+                        document.getElementById('modalResult').innerHTML = '';
+                        for (let i = 0; i < res.wifi.length; i++) {
+                            document.getElementById('modalResult').innerHTML += `<span type="button" class="btn btn-white w-100 align-items-stretch d-flex" style="width: 350px; background-color: #EBEBEB;"><b style="float: left;"><i class="fa fa-wifi" id="wifi_icon" style="color: dodgerblue;"></i> ${res.wifi[i].ssid}</b><b style="float: right;">Connect</b></span>`;
+                        }
                     }
+                    setTimeout(function() {load()}, 3000);
+                } else {
+                    document.getElementById('modalResult').innerHTML = `<span type="button" class="btn btn-white w-100 align-items-stretch d-flex" style="width: 350px; background-color: #EBEBEB;"><center><b style="color: red;">Network Priority</b></center></span>`;
                 }
-                setTimeout(function() {
-                    load();
-                }, 5000);
-            } else {
-                document.getElementById('modalResult').innerHTML = `<span type="button" class="btn btn-white w-100 align-items-stretch d-flex" style="width: 358px; background-color: #EBEBEB;"><center><b style="color: red;">Network Priority</b></center></span>`;
-            }
-        });
+            });
+        }
     }
 }
