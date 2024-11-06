26fd6a80404b7260d43e8e1fa78df8cd5663c043
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2024-11-07 00:00:46 +0700

    Update main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,1 +1,59 @@
+document.getElementById('adbHost').innerHTML = `ADB > ${window.location.host}`;
 
+$(document).ready(function() {
+    $.getJSON('/battery', function(res) {
+        document.getElementById('battery').innerHTML = `${res.percentage}% <i class="fa fa-battery-0" style="color: black"></i>`;
+    });
+
+    $.getJSON('/topButton', function(res) {
+        const mobileDataId = res.btnData === 'on' ? 'off' : 'on';
+        const mobileDataICon = res.btnData === 'on' ? 'font-size: 48px; color: dodgerblue;' : 'font-size: 48px; color: black;';
+        const wifiId = res.btnWifi === 'on' ? 'off' : 'on';
+        const wifiICon = res.btnWifi === 'on' ? 'font-size: 48px; color: dodgerblue;' : 'font-size: 48px; color: black;';
+        const airplaneId = res.btnAirplane === 'on' ? 'off' : 'on';
+        const airplaneICon = res.btnAirplane === 'on' ? 'font-size: 48px; color: dodgerblue;' : 'font-size: 48px; color: black;';
+
+        document.getElementsByName('btnMobileData')[0].setAttribute('id', mobileDataId);
+        document.getElementById('mobileData_icon').setAttribute('style', mobileDataICon);
+        document.getElementsByName('btnWifi')[0].setAttribute('id', wifiId);
+        document.getElementById('wifi_icon').setAttribute('style', wifiICon);
+        document.getElementsByName('btnAirplaneMode')[0].setAttribute('id', airplaneId);
+        document.getElementById('airplaneMode_icon').setAttribute('style', airplaneICon);
+    });
+
+    $.getJSON('/network', function(res) {
+
+    });
+});
+
+$(document).on('click', '[name="mobile_data"]', function() {
+    const onOff = this.id;
+    document.getElementsByName('mobile_data')[0].setAttribute('disabled', '');
+    $.post('/mobile_data', {
+        'set': onOff
+    }, function() {
+        btnLoad();
+    });
+});
+
+$(document).on('click', '[name="wifi"]', function() {
+    const onOff = this.id;
+    document.getElementsByName('wifi')[0].setAttribute('disabled', '');
+    $.post('/wifi', {
+        'set': onOff
+    }, function() {
+        btnLoad();
+    });
+});
+
+$(document).on('click', '[name="airplane"]', function() {
+    const onOff = this.id;
+    document.getElementsByName('airplane')[0].setAttribute('disabled', '');
+    $.post('/airplane', {
+        'set': onOff
+    }, function() {
+        btnLoad();
+    });
+});
+
+function updateBtn
