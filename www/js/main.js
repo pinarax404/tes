f6d49ee2ee91882e74188ff4a64eeb184a07eeb3
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2024-11-07 02:51:19 +0700

    Update main.js

--- www/js/main.js
+++ www/js/main.js
@@ -80,7 +80,7 @@ function updateWifiList(res) {
     if (res.status === 'ok') {
         document.getElementById('modalResult').innerHTML = '';
         for (let i = 0; i < res.wifi.length; i++) {
-            
+            document.getElementById('modalResult').innerHTML += `<span type="button" class="btn btn-white w-100 align-items-stretch d-flex" style="width: 358px; background-color: #EBEBEB;"><b style="float: left;"><i class="fa fa-wifi" id="wifi_icon" style="color: dodgerblue;"></i> ${res.wifi[i].ssid}</b><b style="float: right;">Connect</b></span>`;
         }
     } else {
         document.getElementById('modalResult').innerHTML = `<span type="button" class="btn btn-white w-100 align-items-stretch d-flex" style="width: 358px; background-color: #EBEBEB;"><center><b style="color: red;">Network Priority</b></center></span>`;
