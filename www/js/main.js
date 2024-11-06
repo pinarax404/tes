fba9631554af616b13e479f19a958ccd236c41d3
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2024-11-07 06:37:01 +0700

    Update main.js

--- www/js/main.js
+++ www/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 });
 
 $(document).on('click', '[id="btnMobileData"]', function() {
-    const onOff = this.name;
+    const onOff = document.getElementById('btnMobileData').getAttribute('name');
     document.getElementById('btnMobileData').setAttribute('disabled', '');
     $.post('/switchButton', {'name': 'mobileData', 'state': onOff}, function() {
         $.getJSON('/topButton', function(res) {
@@ -44,7 +44,7 @@ $('#btnWifi').on('mouseup', function() {
 });
 
 $(document).on('click', '[id="btnAirplaneMode"]', function() {
-    const onOff = this.name;
+    const onOff = document.getElementById('btnMobileData').getAttribute('name');
     document.getElementById('btnAirplaneMode').setAttribute('disabled', '');
     $.post('/switchButton', {'name': 'airplane', 'state': onOff}, function() {
         $.getJSON('/topButton', function(res) {
